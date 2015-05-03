7fffb3ee6ba0772da683295e251fff3bbb4ee07d
ravsa <rvsbana@gmail.com>
2015-05-03 12:00:55 +0530

    fix some bugs

--- tilone.py
+++ tilone.py
@@ -1,6 +1,7 @@
 from subprocess import *
 import sys
-child='curl '+'-u '+"'"+sys.argv[1]+"'"+':'+"'"+sys.argv[2]+"'"+' https://api.github.com/user/repos '+'-d '+'"'+'{'+'\\"name\\"'+':'+'\\"'+sys.argv[2]+'\\"'+'}'+'"'
-cho,chr=Popen(child,shell=True,stdout=PIPE,stderr=PIPE).communicate();
-print cho ,chr
+child='curl '+'-u '+"'"+sys.argv[1]+"'"+':'+"'"+sys.argv[2]+"'"+' https://api.github.com/user/repos '+'-d '+'"'+'{'+'\\"name\\"'+':'+'\\"'+sys.argv[3]+'\\"'+'}'+'"'
+cho=call(child,shell=True,stderr=PIPE)
+if cho:
+	print "Error in communication"
 
