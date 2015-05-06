4e0510e5c7bed2fac21eca2fb7d6d67b732797e5
ravsa <rvsbana@gmail.com>
2015-05-06 17:02:30 +0530

    add function to commit and add button

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import tkMessageBox as tb
 import sys,subprocess,os
 root,branch,noncommit,committed,statlab,stat,delete=None,None,'','',None,None,''
 frame1,frame2,frame3,commit=None,None,None,None
-
+username,passwd,reponame='','',''
 def gitstatus(event):
 	global noncommit,committed,stat,delete,branch
 	noncommit,committed,delete,branch='','','',''
@@ -24,32 +24,50 @@ def gitstatus(event):
 		if 'deleted:' in i:
 			delete=delete+'\n'+i
 	stat.set("""[STATUS]\n\n<BRANCH>:%s\n<NOT 2B COMMITTED>: %s\n<2B COMMITTED>: %s\n<DELETED>: %s"""%(branch,noncommit,committed,delete))	
+def check():
+	global username,passwd
+	def warn(strng):
+		tb.showerror('error',strng)
+	if username is '':
+		warn("username field is empty")
+		return 1
+	if passwd is '':
+		warn('password field is empty')
+		return 1
+	return 0
 def create_repo():
 	er=os.system('python tilone.py')
 	if er is not 0:
 		tb.showerror("","Python tilone.py module not found")
 def cmt():
 	global commit
-	new=tk.Tk()
-	
-#	commit=
-	commit='git '+'commit -m '+'"'+commit+'"'
-	
+	new=tk.Tk()			
+	gk=tk.Text(new,width=35,height=10)
+	gk.pack()
+	def com():
+		commit=gk.get(0.0,'end')
+		commit='git '+'commit -m '+'"'+commit[:-1]+'"'
+		err=os.system(commit)
+		if err:
+			tb.showinfo('success','Successfully commited !')
+		else:
+			tb.showerror('failed','Failed to commit!')
+	tk.Button(new,text='  DONE  ',command=com).pack()
 def add():
 	os.system('git add *')
 	gitstatus('')
 def inpOpt():
-	global frame2,frame3
+	global frame2,frame3,username,passwd,reponame
 	tk.Label(frame2,text='username',height=3,font=tf.Font(family='halvetica',size=15)).grid(rowspan=2,row=0,column=0)
 	tk.Label(frame2,text="password",height=3,font=tf.Font(family='halvetica',size=15)).grid(rowspan=2,row=2,column=0)
 	tk.Label(frame2,text="Repositoy",height=3,font=tf.Font(family='halvetica',size=15)).grid(rowspan=2,row=4,column=0)
-	tl.username=tk.Entry(frame2)
-	tl.username.grid(row=0,column=1,stick='s')
-	tl.passwd=tk.Entry(frame2)
-	tl.passwd.grid(row=2,column=1,stick='s')
-	tl.repository=tk.Entry(frame2,foreground='darkgreen')
-	tl.repository.grid(row=4,column=1,stick='s')
-	tl.repository.insert(0,tl.currentDir())
+	use=tk.Entry(frame2)
+	use.grid(row=0,column=1,stick='s')
+	pwd=tk.Entry(frame2)
+	pwd.grid(row=2,column=1,stick='s')
+	renme=tk.Entry(frame2,foreground='darkgreen')
+	renme.grid(row=4,column=1,stick='s')
+	renme.insert(0,tl.currentDir())
 	tk.Button(frame3,foreground='white',activebackground='blue',bg='black',text='NewRepo',command=create_repo,width=10,height=2).grid(row=0,column=0,stick='n')
 	tk.Button(frame3,foreground='white',activebackground='blue',bg='black',text='  Add  ',command=add,width=10,height=2).grid(row=1,column=0,stick='n')
 	tk.Button(frame3,foreground='white',activebackground='blue',bg='black',text='Commit',command=cmt,width=10,height=2).grid(row=2,column=0,stick='n')
