054d212e1a2fbd259d65f77701175800466b2bec
rvasa <rvsbana@gmail.com>
2015-05-07 13:48:36 +0530

    bugs remove

--- main.py
+++ main.py
@@ -131,9 +131,6 @@ def init():
 	else:
 		os.system('git init')
 		i=tb.showinfo('','Initialized')
-def add():
-	o=subprocess.Popen('git add *',shell=True,stdout=subprocess.PIPE).communicate()[0]
-	gitstatus('')
 def inpOpt():
 	global frame2,frame3,use,pwd,renme
 	tk.Label(frame2,text='username',height=3,font=tf.Font(family='halvetica',size=15)).grid(rowspan=2,row=0,column=0)
@@ -149,7 +146,10 @@ def inpOpt():
 	tk.Button(frame3,foreground='white',activebackground='blue',bg='black',text='Init',command=init,width=10,height=2).grid(row=0,column=0,stick='n')
 
 	tk.Button(frame3,foreground='white',activebackground='blue',bg='black',text='NewRepo',command=create_repo,width=10,height=2).grid(row=1,column=0,stick='n')
-	tk.Button(frame3,foreground='white',activebackground='blue',bg='black',text='  Add  ',command=add,width=10,height=2).grid(row=2,column=0,stick='n')
+	def pad():
+		os.system('git add *')
+		gitstatus('')
+	tk.Button(frame3,foreground='white',activebackground='blue',bg='black',text='  Add  ',command=pad,width=10,height=2).grid(row=2,column=0,stick='n')
 	tk.Button(frame3,foreground='white',activebackground='blue',bg='black',text='Commit',command=cmt,width=10,height=2).grid(row=3,column=0,stick='n')
 	tk.Button(frame3,foreground='white',activebackground='blue',bg='black',text='Push',command=push,width=10,height=2).grid(row=4,column=0,stick='n')
 	tk.Button(frame3,foreground='white',activebackground='blue',bg='black',text='terminal',command=term,width=10,height=2).grid(row=5,column=0,stick='n')
